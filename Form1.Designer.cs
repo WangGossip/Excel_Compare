cd577a0d98d5559baaf3cc1510a47e0b36405156
unknown <982728182@qq.com>
2019-12-29 23:22:28 +0800

    第一次提交

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Excel_Compare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.使用帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.选择原始文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.选择被比较文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.选择输出文件夹ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.使用帮助ToolStripMenuItem,
+            this.选择原始文件ToolStripMenuItem,
+            this.选择被比较文件ToolStripMenuItem,
+            this.选择输出文件夹ToolStripMenuItem,
+            this.开始ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 使用帮助ToolStripMenuItem
+            // 
+            this.使用帮助ToolStripMenuItem.Name = "使用帮助ToolStripMenuItem";
+            this.使用帮助ToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.使用帮助ToolStripMenuItem.Text = "使用帮助";
+            this.使用帮助ToolStripMenuItem.Click += new System.EventHandler(this.使用帮助ToolStripMenuItem_Click);
+            // 
+            // 选择原始文件ToolStripMenuItem
+            // 
+            this.选择原始文件ToolStripMenuItem.Name = "选择原始文件ToolStripMenuItem";
+            this.选择原始文件ToolStripMenuItem.Size = new System.Drawing.Size(113, 24);
+            this.选择原始文件ToolStripMenuItem.Text = "选择原始文件";
+            this.选择原始文件ToolStripMenuItem.Click += new System.EventHandler(this.选择原始文件ToolStripMenuItem_Click);
+            // 
+            // 选择被比较文件ToolStripMenuItem
+            // 
+            this.选择被比较文件ToolStripMenuItem.Name = "选择被比较文件ToolStripMenuItem";
+            this.选择被比较文件ToolStripMenuItem.Size = new System.Drawing.Size(128, 24);
+            this.选择被比较文件ToolStripMenuItem.Text = "选择被比较文件";
+            this.选择被比较文件ToolStripMenuItem.Click += new System.EventHandler(this.选择被比较文件ToolStripMenuItem_Click);
+            // 
+            // 选择输出文件夹ToolStripMenuItem
+            // 
+            this.选择输出文件夹ToolStripMenuItem.Name = "选择输出文件夹ToolStripMenuItem";
+            this.选择输出文件夹ToolStripMenuItem.Size = new System.Drawing.Size(128, 24);
+            this.选择输出文件夹ToolStripMenuItem.Text = "选择输出文件夹";
+            this.选择输出文件夹ToolStripMenuItem.Click += new System.EventHandler(this.选择输出文件夹ToolStripMenuItem_Click);
+            // 
+            // 开始ToolStripMenuItem
+            // 
+            this.开始ToolStripMenuItem.Name = "开始ToolStripMenuItem";
+            this.开始ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.开始ToolStripMenuItem.Text = "开始";
+            this.开始ToolStripMenuItem.Click += new System.EventHandler(this.开始ToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "当前选择的输出文件夹为：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "当前选择的原始文件为：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(195, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "当前选择的被比较文件为：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(210, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 15);
+            this.label6.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 使用帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 选择原始文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 选择被比较文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 选择输出文件夹ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开始ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace Excel_Compare
+{
+    public partial class Form1 : Form
+    {
+        /*常量*/
+        private static int key_id_row_num_new = 0;//原始文件主码所在列
+        private static int key_id_row_num_old = 0;//新文件主码所在列
+        /*变量区*/
+        private string old_file_name, new_file_name, out_file_folder, out_file1, out_file2;
+        private List<string> old_colums, new_colums, old_rows, new_rows;//行、列名称
+        private List<int> old_column_stas, new_column_stas, old_row_stas, new_row_stas;//行、列状态数组-1：另一边不存在（多余）;1:另一边存在(直接标记对应下标)
+        //List<int> same_old, same_new;
+
+        private void 选择被比较文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*选择并打开文件，读取数据，关闭文件*/
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                Title = "请选择一个符合格式的excel表格",
+                Filter = "Excel文件(*.xlsx)|*.xlsx"
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                new_file_name = dialog.FileName;
+            }
+            dialog.Dispose();
+            label6.Text = new_file_name;
+        }
+
+        private void 选择原始文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*选择并打开文件，读取数据，关闭文件*/
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                Title = "请选择一个符合格式的excel表格",
+                Filter = "Excel文件(*.xlsx)|*.xlsx"
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                old_file_name = dialog.FileName;
+            }
+            dialog.Dispose();
+            label4.Text = new_file_name;
+        }
+
+        private void 使用帮助ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void 选择输出文件夹ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择目标文件夹";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+            }
+            out_file_folder = dialog.SelectedPath;
+            dialog.Dispose();
+            label2.Text = out_file_folder;
+        }
+        
+        protected bool Judge_Input_Full()
+        {
+            bool input_ok = true;
+            if (old_file_name == "")
+            {
+                input_ok = false;
+                MessageBox.Show("清选择原始文件！");
+            }
+            else if(new_file_name=="")
+            {
+                input_ok = false;
+                MessageBox.Show("清选择待比较文件！");
+            }
+            else if(out_file_folder=="")
+            {
+                input_ok = false;
+                MessageBox.Show("清选择输出文件夹！");
+            }
+            return input_ok;
+        }
+
+        /// <summary>
+        /// 将资源文件复制到目标文件夹，进行修改
+        /// </summary>
+        protected void Copy_Original_Files()
+        {
+            out_file1 = out_file_folder + "\\file1.xlsx";
+            out_file2 = out_file_folder + "\\file2.xlsx";
+            //删掉原有文件
+            if (File.Exists(out_file1))
+                File.Delete(out_file1);
+            if (File.Exists(out_file2))
+                File.Delete(out_file2);
+            //复制文件
+            File.Copy(old_file_name, out_file1);
+            File.Copy(new_file_name, out_file2);
+            return ;
+        }
+        private void 开始ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*变量区*/
+            int old_col_num, new_col_num, old_row_num, new_row_num, i, j;
+            FileStream fs1, fs2;
+            IWorkbook wk1,wk2;
+            ISheet sheet1, sheet2;
+            IRow row1, row2;
+            if (!Judge_Input_Full())//判断输入
+                return ;
+            Copy_Original_Files();//先复制旧的文件到新的文件夹中
+            //打开文件,获取基础参数
+            fs1 = File.OpenRead(out_file1);
+            fs2 = File.OpenRead(out_file2);
+            wk1 = new XSSFWorkbook(fs1);
+            wk2 = new XSSFWorkbook(fs2);
+            fs1.Close();
+            fs2.Close();
+            sheet1 = wk1.GetSheetAt(0);
+            sheet2 = wk2.GetSheetAt(0);
+            old_row_num = sheet1.LastRowNum + 1;//总行数1
+            new_row_num = sheet2.LastRowNum + 1;//总行数2
+            row1 = sheet1.GetRow(0);
+            row2 = sheet2.GetRow(0);
+            old_col_num = row1.LastCellNum;//总列数1
+            new_col_num = row2.LastCellNum;//总列数2
+            //获取列名称
+            for (i = 0; i < old_col_num; i++)
+                old_colums.Add(row1.GetCell(i).ToString());
+            for (i = 0; i < new_col_num; i++)
+                new_colums.Add(row2.GetCell(i).ToString());
+            //比较列名
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            old_colums = new List<string>();
+            new_colums = new List<string>();
+            old_rows = new List<string>();
+            new_rows = new List<string>();
+        }
+    }
+}
